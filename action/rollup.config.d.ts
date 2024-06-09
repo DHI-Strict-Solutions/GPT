dc27d4daac9d1dab779c87f37bb6122276e18e9d
DRdarwin <31813699+DRdarwin@users.noreply.github.com>
2024-06-09 18:44:23 +0300

    Add files via upload
    Signed-off-by: DRdarwin <31813699+DRdarwin@users.noreply.github.com>

--- action/rollup.config.d.ts
+++ action/rollup.config.d.ts
@@ -1,2 +1,29 @@
-declare const _default: import("rollup").RollupOptions[];
-export default _default;
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import esbuild from 'rollup-plugin-esbuild';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'esm',
+    sourcemap: true
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    json(),
+    esbuild({
+      include: /\.[jt]sx?$/,
+      exclude: /node_modules/,
+      sourceMap: true,
+      minify: process.env.NODE_ENV === 'production',
+      target: 'es2017',
+      tsconfig: 'tsconfig.json',
+      loaders: {
+        '.json': 'json'
+      }
+    })
+  ]
+};
